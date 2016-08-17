51506b376e6109af739d4c17e933bb8ca5039ef2
nurhidayat-agung <agung7659@students.amikom.ac.id>
2016-08-17 19:44:33 +0700

    initial commit

---
+++ app/src/main/java/com/example/kazt/listview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.kazt.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    public static final String[] androidVersion = new String[]{
+            "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "Kitkat", "Lolipop"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,androidVersion);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
